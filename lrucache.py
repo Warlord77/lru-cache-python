ff3657a3a09a7de2955f690a9d1ac92f5483fc01
One2N <one2n@One2Ns-MacBook-Air.local>
2025-04-05 19:00:09 +0530

    added new implementation

--- lrucache.py
+++ lrucache.py
@@ -1,35 +1,34 @@
-cache = {}  
-recently_used = []  
-max_size = 3
+cache = {}
+frequency = {}
+max_size = 5
 
-def get(key):  
-    if key in cache:  
-        recently_used.remove(key)  
-        recently_used.append(key)  
-        return cache[key]  
-    else:  
-        return None  
+def get(key):
+    if key in cache:
+        frequency[key] += 1
+        return cache[key]
+    else:
+        return None
 
-def put(key, value):  
-    if key in cache:  
-        cache[key] = value  
-        recently_used.remove(key) 
-        recently_used.append(key)  
-    else:  
-        if len(cache) >= max_size:  
-            oldest_key = recently_used.pop(0)  
-            del cache[oldest_key]  
-        cache[key] = value  
-        recently_used.append(key)  
+def put(key, value):
+    if key in cache:
+        cache[key] = value
+        frequency[key] += 1
+    else:
+        if len(cache) >= max_size:
+            least_used_key = min(frequency, key=frequency.get)
+            del cache[least_used_key]
+            del frequency[least_used_key]
+        cache[key] = value
+        frequency[key] = 1
 
-def remove(key):  
-    if key in cache:  
-        del cache[key]  
-        recently_used.remove(key)  
-        return True  
-    return False  
+def remove(key):
+    if key in cache:
+        del cache[key]
+        del frequency[key]
+        return True
+    return False
 
-def show_cache():  
-    print("Cache contents:", cache)  
-    print("Usage order (oldest to newest):", recently_used)  
+def show_cache():
+    print("Cache contents:", cache)
+    print("Access frequency:", frequency)
     return cache
